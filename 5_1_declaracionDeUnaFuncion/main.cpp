b6c5f180d5fff62f97d891051ef24d16623a9063
AlejandroColl006 <acolllen@myuax.com>
2024-10-30 19:49:11 +0100

    wip

---
+++ 5_1_declaracionDeUnaFuncion/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+
+int suma (int a, int b) {
+    return a+b;
+}
+
+int main() {
+    int a = 2;
+    std::cout << "El resultado de la suma es: " << suma(a, 3);
+   return 0;
+}
