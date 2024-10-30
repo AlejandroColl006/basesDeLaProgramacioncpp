e1b40913405fb5aea429fc14edebf0343d341ad1
AlejandroColl006 <acolllen@myuax.com>
2024-10-30 18:57:39 +0100

    wip

---
+++ 3_4_instruccionesDeSalto/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+
+int main() {
+    int numero;
+    int total = 0;
+    while (true) {
+        std::cout << "Ingrese un numero (presiona 0 para termianr): ";
+        std::cin >> numero;
+        if (numero == 0) {
+            break;
+        }
+        if (numero > 0) {
+            total += numero;
+            std::cout << "Total calculado: " << total << std::endl;
+        }
+    }
+    return 0;
+}
