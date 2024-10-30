3287d46ebaf33d6a31744ebee17202d16a459580
AlejandroColl006 <acolllen@myuax.com>
2024-10-30 19:13:19 +0100

    wip

--- 3_4_instruccionesDeSalto/main.cpp
+++ 3_4_instruccionesDeSalto/main.cpp
@@ -5,7 +5,7 @@ int main() {
     int numero;
     int total = 0;
     while (true) {
-        std::cout << "Ingrese un numero (presiona 0 para termianr): ";
+        std::cout << "Ingrese un numero (presiona 0 para terminar): ";
         std::cin >> numero;
         if (numero == 0) {
             break;
