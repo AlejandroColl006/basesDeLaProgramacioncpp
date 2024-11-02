d6743d7fbc6b41015959f0f8af54aa8ccbcf4c3a
AlejandroColl006 <acolllen@myuax.com>
2024-11-02 11:05:21 +0100

    wip

--- 6_4_aritmeticaDeLosPunteros/main.cpp
+++ 6_4_aritmeticaDeLosPunteros/main.cpp
@@ -1,30 +1,27 @@
 #include <iostream>
 
 int main() {
-    int array[] = {10, 20, 30, 40, 50};
-    int tamaño = sizeof(array) / sizeof(array[0]);
+    //Lo primero es establecer un tamaño de array (lista de números)
+    const int espacioDeTamanio = 12;
+    int array[espacioDeTamanio] = {5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60,};
 
-    int *puntero = array;
+    int *ptr = array;
+    //En este paso indicamos que el valor del puntero va a ser un número entero indicados en el array
+    std::cout << "Valores del array utilizando aritmética de punteros:" << std::endl;
 
-    int **punteroAPuntero = &puntero;
+    for (int i = 0; i < espacioDeTamanio; ++i) {
+        std::cout << "array[" << i << "] = " << *(ptr + i) << std::endl;
+    }
+    for (int i = 0; i < espacioDeTamanio; ++i) {
+        *(ptr + i) += 5;
+    }
+    //En el paso siguiente se mostrara los valores del array tras haberlo modificado en el bucle anterior
+    std::cout << "Valores del array después de la modificación:" << std::endl;
 
-    std::cout << "Valores del array usando punteros a punteros:" << std::endl;
-
-    for (int i = 0; i < tamaño; ++i) {
-        std::cout << "array[" << i << "] = " << *(*punteroAPuntero + i) << std::endl;
+    for (int i = 0; i < espacioDeTamanio; ++i) {
+        std::cout << "array[" << i << "] = " << *(ptr + i) << std::endl;
     }
 
-
-    for (int i = 0; i < tamaño; ++i) {
-        *(*punteroAPuntero + i) += 5;
-    }
-
-    std::cout << "\nValores del array después de la modificación:" << std::endl;
-
-
-    for (int i = 0; i < tamaño; ++i) {
-        std::cout << "array[" << i << "] = " << *(*punteroAPuntero + i) << std::endl;
-    }
 
     return 0;
 }
